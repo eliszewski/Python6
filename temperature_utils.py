6d4229bee9ba8db4585ef12de1922fd746167b19
eliszewski <liszewski31098@gmail.com>
2023-03-20 15:19:46 -0400

    parts 1,2

--- temperature_utils.py
+++ temperature_utils.py
@@ -8,7 +8,7 @@ def convert_to_celsius(fahrenheit_temp: float) -> float:
     :param fahrenheit_temp: A float representing a temperature in fahrenheit
     :return: A float representing the corresponding value of the fahrenheit_temp parameter in celsius
     """
-    pass  # remove pass statement and implement me
+    return round(((fahrenheit_temp - 32) * 5 / 9), 2)
 
 
 def convert_to_fahrenheit(celsius_temp: float) -> int:
@@ -18,7 +18,7 @@ def convert_to_fahrenheit(celsius_temp: float) -> int:
     :param celsius_temp: A float representing a temperature in celsius
     :return:  A float representing the corresponding value of the celsius_temp parameter in fahrenheit
     """
-    pass  # remove pass statement and implement me
+    return round(((celsius_temp * 9 / 5) + 32), 2)
 
 
 def temperature_tuple(temperatures: Iterable, input_unit_of_measurement: str) -> Tuple[Tuple[float, float]]:
@@ -31,4 +31,11 @@ def temperature_tuple(temperatures: Iterable, input_unit_of_measurement: str) ->
     :param input_unit_of_measurement: The unit a measure to use to convert the values in the temperatures parameter
     :return: A tuple of tuples
     """
-    pass  # remove pass statement and implement me
+    temps = ()
+    if (input_unit_of_measurement == 'c'):
+        for index, element in enumerate(temperatures):
+            temps += (element, convert_to_fahrenheit(element)),
+    elif (input_unit_of_measurement == 'f'):
+        for index, element in enumerate(temperatures):
+            temps += (element, convert_to_celsius(element)),
+    return temps
